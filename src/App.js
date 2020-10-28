af439d62c60c4a7ae8ebeb3ac50392ae6d9d2eb7
Paul Buysse <paulbuysse@Pauls-MacBook-Pro.local>
2020-10-28 09:42:28 -0500

    added some animation tests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+
+import Header from './Header.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and asdfsave to reload.
           </p>
           <a
             className="App-link"
