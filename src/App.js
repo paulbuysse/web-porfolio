22af4bd051bb0028f3f9fadac777d1161dccf3ab
Paul Buysse <paulbuysse@Pauls-MacBook-Pro.local>
2020-11-08 14:35:25 -0600

    got working slides, resized

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import Header from './Header.js'
+import Footer from './Footer.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Footer />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
