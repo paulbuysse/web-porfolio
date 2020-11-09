5260f5e8b883134cadbe711f1a7afd59d800941d
Paul Buysse <paulbuysse001@gmail.com>
2020-11-08 22:10:30 -0600

    played with images

--- src/App.js
+++ src/App.js
@@ -13,18 +13,7 @@ class App extends Component {
         <Header />
         <Footer />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and asdfsave to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
         </header>
       </div>
     );
--- src/Footer.js
+++ src/Footer.js
@@ -4,10 +4,10 @@ import { useTransition, animated, config } from 'react-spring'
 import './App.css'
 
 const slides = [
-  { id: 0, url: 'photo-1544511916-0148ccdeb877?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1901&q=80i' },
-  { id: 1, url: 'photo-1544572571-ab94fd872ce4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1534&q=80' },
-  { id: 2, url: 'reserve/bnW1TuTV2YGcoh1HyWNQ_IMG_0207.JPG?ixlib=rb-1.2.1&w=1534&q=80' },
-  { id: 3, url: 'photo-1540206395-68808572332f?ixlib=rb-1.2.1&w=1181&q=80' },
+  { id: 0, url: 'https://i.imgur.com/j6H8gxq.jpg' },
+  { id: 1, url: 'https://i.imgur.com/StV946k.jpg' },
+  { id: 2, url: 'https://i.imgur.com/YBZacyX.jpg' },
+  { id: 3, url: 'https://i.imgur.com/bqUT4kP.jpg' },
 ]
 
 const Footer = () => {
@@ -23,10 +23,12 @@ const Footer = () => {
     <animated.div
       key={key}
       class="bg"
-      style={{ ...props, backgroundImage: `url(https://images.unsplash.com/${item.url}&auto=format&fit=crop)` }}
-    />
+      style={{ ...props, backgroundImage: `${item.url}` }}
+    >
+        <img className="imgSlide" src={item.url} />
+    </animated.div>
   ))
 }
 
-render(<Footer />, document.getElementById('root'))
+//render(<Footer />, document.getElementById('root'))
 export default Footer;
